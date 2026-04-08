f1414514cd595d85d10b9e0e728d6850eb43c490
George George <156202950+GeorgieGeorge-jr@users.noreply.github.com>
2026-04-08 05:16:02 +0100

    ..

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5050/api"
-    : "https://arclist-production.up.railway.app/api";
+    : "https://arclist-production.up.railway.app";
 
 export { API_BASE_URL };
